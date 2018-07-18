47fee551abb6a86cc03b1d1e4ecf6bbfeb8d215f
CongNT <congnt@viosoft.com>
2018-07-18 16:47:40 +0700

    Allow using ssh key to clone
    Signed-off-by: CongNT <congnt@viosoft.com>

--- docker-arm/make_inside.sh
+++ docker-arm/make_inside.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-#USERNAME=
-#PASSWORD=
 
-git clone https://$USERNAME:$PASSWORD@github.com/viosoft-corp/dronava.git
+git clone git@github.com:viosoft-corp/dronava.git
 cd dronava/components/openairinterface5g/cmake_targets/
 git checkout feature_oai_light
 ./build_oai -I

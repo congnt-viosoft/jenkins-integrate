b4370a3a74bd334df0b2384f457152eb24d0f392
CongNT <congnt@viosoft.com>
2018-07-20 17:13:48 +0700

    Use local script instead
    Signed-off-by: CongNT <congnt@viosoft.com>

--- docker-arm/make_inside.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-git clone git@github.com:viosoft-corp/dronava.git
-cd dronava/components/openairinterface5g/cmake_targets/
-git checkout feature_oai_light
-./build_oai -I
-./build_oai --fido-enb --arm-ap --enable-x2
